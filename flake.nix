2f434ccf4e697bc1472819d24149cd37f79904e5
Alister Machado <alister.reis@gmail.com>
2026-09-16 10:44:35 -0300

    use flake for dev env

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+    description = "Development environment for Tinyrenderer (https://haqr.eu/tinyrenderer/)";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
+
+    outputs = {self, nixpkgs}:
+        let
+            system = "aarch64-darwin";
+            pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+            devShells.${system}.default = pkgs.mkShell {
+                buildInputs = with pkgs; [
+                    cmake
+                ];
+            };
+        };
+}
