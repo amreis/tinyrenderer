c0022c3e70aad55d0c14af12aa104d6096123541
Alister Machado <alister.reis@gmail.com>
2026-09-16 11:09:13 -0300

    progress

--- main.cpp
+++ main.cpp
@@ -7,6 +7,22 @@ constexpr TGAColor red = {0, 0, 255, 255};
 constexpr TGAColor blue = {255, 128, 64, 255};
 constexpr TGAColor yellow = {0, 200, 255, 255};
 
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color)
+{
+    float dx, dy;
+    dx = bx - ax;
+    dy = by - ay;
+
+    float cur_x = ax, cur_y = ay;
+
+    for (float t = 0.0; t <= 1.0; t += 0.02)
+    {
+        int x = std::round(ax + dx * t);
+        int y = std::round(ay + dy * t);
+        framebuffer.set(x, y, color);
+    }
+}
+
 int main(int argc, char **argv)
 {
     constexpr int width = 64;
@@ -17,9 +33,10 @@ int main(int argc, char **argv)
     int bx = 12, by = 37;
     int cx = 62, cy = 53;
 
-    framebuffer.set(ax, ay, white);
-    framebuffer.set(bx, by, white);
-    framebuffer.set(cx, cy, white);
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
 
     framebuffer.write_tga_file("framebuffer.tga");
     return 0;
